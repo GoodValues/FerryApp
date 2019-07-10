f7757915f71bd06340453a5bbf4edd190b645fc7
Unas <9931422+Unasbj@users.noreply.github.com>
2019-07-10 17:23:27 +0200

    dodatkowe zasoby

--- src/main/java/pl/exercise/ferry/ticket/Ticket.java
+++ src/main/java/pl/exercise/ferry/ticket/Ticket.java
@@ -1,5 +1,8 @@
 package pl.exercise.ferry.ticket;
 
+import java.math.BigDecimal;
+
 public interface Ticket {
 
+  BigDecimal getPrice();
 }
---
+++ src/main/java/pl/exercise/ferry/ticket/VehicleTicket.java
@@ -0,0 +1,21 @@
+package pl.exercise.ferry.ticket;
+
+import java.math.BigDecimal;
+import pl.exercise.ferry.vehicle.VehicleType;
+
+public abstract class VehicleTicket implements Ticket {
+
+  private final VehicleType vehicleType;
+  private final String owner;
+
+  VehicleTicket(VehicleType vehicleType, String owner) {
+    this.vehicleType = vehicleType;
+    this.owner = owner;
+  }
+
+  @Override
+  public BigDecimal getPrice() {
+    return BigDecimal.ZERO;
+  }
+
+}
